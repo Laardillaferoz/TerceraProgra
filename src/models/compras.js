d1c52df8a454b12f324c93e707cbb62c6bd0a57a
ManuelChaves <manuelchavesmoya@gmail.com>
2020-12-11 22:40:48 -0600

    Avance en compras
    Masi me ama

--- src/models/compras.js
+++ src/models/compras.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const SchemaCompra = new Schema({
-    ProductoCompra: {type: String, required:true },
-    Cantidad: {type: String, required:true },
+    cliente:{
+        type: String,
+    },
+    products:{
+        type: Array,
+        default: []
+    },
+    precioFinal:{
+        type: Number,
+        default:0
+    }
+    //ProductoCompra: {type: String, required:true },
+    //Cantidad: {type: String, required:true },
 });
 
 module.exports=mongoose.model('compras', SchemaCompra);
--- src/routes/compras.js
+++ src/routes/compras.js
@@ -4,7 +4,7 @@ const compras = require('../models/compras');
 const productosDisponibles = require('../models/productoModel');
 const router = express.Router();
 
-router.get('/mostrar/compra', (req, res) => {
+router.get('/compras/registrarCompra', (req, res) => {
     res.render('compras/registrarCompra');
 });
 
@@ -15,20 +15,24 @@ router.post('/compras/registrarCompra', async (req, res) => {
     var errors = [];
     var success = [];
 
+    console.log('hola1');
     if (! ProductoCompra) {
+        console.log('ella');
         req.flash('error_msg' , "Ingrese el producto");
     }
     if (! Cantidad) {
-        req.flash('error_msg' , "Cantidad ples");
+        console.log('NO me');
+        errors.push({text:"You must enter the quantity"});
     }
     if (errors.length > 0) {
+        console.log('me ama');
         res.render("./compras/registrarCompra", {errors});
     } else {
-
+        console.log('aquo');
         var productosD = require('../index').compraActual.ProductoCompra;
 
         await productosDisponibles.findOne({
-            nombreArticulo: productosD
+            ProductoCompra: ProductoCompra
         }, async (err, found) => {
             if (!found) {
                 req.flash('error_msg' , "El producto NO existe");
@@ -40,11 +44,11 @@ router.post('/compras/registrarCompra', async (req, res) => {
                 } else {
                     var Actual  = require('../index').compraActual.precioFinal;
                     var precio = (parseInt(found.Precio) * parseInt(Cantidad));
-
-                    require('../index').compraActual.nombreArticulo=ProductoCompra; 
-                    require('../index').compraActual.Cantidad= Cantidad;
+                    
+                    require('../index').compraActual.products.push({ProductoCompra:ProductoCompra,Cantidad:Cantidad,unitPrice:found.precio}); 
+                    require('../index').compraActual.precioFinal= (parseInt(Actual)+parseInt(precio))
                     //require('../index').compraActual.Precio = (parseInt(compraActual) + parseInt(Precio));
-
+                    console.log('hola');
                     req.flash('success_msg', 'Producto registrado');
                     res.render("./compras/registrarCompra", {success});
                 }
@@ -54,4 +58,37 @@ router.post('/compras/registrarCompra', async (req, res) => {
     }
 })
 
+router.post('/compras/finalCompra',async(req,res)=>{
+    var success=[];
+
+    require('../index').compraActual.save();
+
+    var i=0;
+    var largo=require('../index').compraActual.products.length;
+
+    while(i<largo){
+        var ProductoCompra=require('../index').compraActual.products[i].ProductoCompra;
+        var Inventario=require('../index').compraActual.products[i].Inventario;
+
+
+        await productosDisponibles.findOne({ProductoCompra:ProductoCompra},function(err,resp){
+            if(err){
+                console.log(err);
+            }else{
+                var cantidadvieja=resp.Inventario;
+                console.log(cantidadvieja)
+                var cantidadnueva=(parseInt(cantidadvieja)-parseInt(Inventario));
+                resp.Inventario=cantidadnueva;
+                console.log(cantidadnueva)
+                resp.save();
+            }
+        });
+        i++;
+    }
+    success.push({text:'Se ha registrado el producto, el precio final es: '+require('../index').compraActual.precioFinal});
+    res.render('./index',{success})
+})
+ router.get('/compras/finalCompra',(req,res)=>{
+     res.render('compras/registrarCompra')
+ })
 module.exports = router;
